29e73acbae32d75fc9d9c6053b33946a5454578a
Harshit <harshitpanchalb@gmail.com>
2024-06-04 22:33:12 +0530

    structure edited

---
+++ Chp 3 to do..java
@@ -0,0 +1,29 @@
+Chp 3..java
+Inheritance definition..syntax and example 
+Benefit of java inheritance 
+
+Types of Inheritance (imp)
+Simple multilevel hierarchical (daigram, syntax, example)
+
+Method overloading (definition and example)
+
+Constructor overloading
+
+Method overriding 
+
+Dynamic Method dispatch (imp)
+With definition and example 
+
+Final keyword, final variable,final Method,final class,
+
+Use of super keyword
+
+Abstract class and abstract Method
+
+Static keyword use
+
+Pg 17 interface  definition syntax and example 
+
+Pg no 19 multiple Inheritance 
+
+Package definition and types
